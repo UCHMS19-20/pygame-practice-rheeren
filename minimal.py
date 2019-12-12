cc5671263612c051a0dc9ef2cb25ca98712dc152
Rachel Heeren <rheeren@ucvts.org>
2019-12-12 10:43:36 -0500

    Update minimal.py

--- minimal.py
+++ minimal.py
@@ -5,13 +5,33 @@ import pygame
 pygame.init()
 
 # Create a display. Size must be a tuple, which is why it's in parentheses
+red = pygame.Color(255,0,0)
+orange = pygame.Color(255,128,0)
+yellow = pygame.Color(255,255,0)
+green = pygame.Color(0,255,0)
+blue =  pygame.Color(0,0,255)
+purple =  pygame.Color(255,0,255)
+colors = [red, orange, yellow, green, blue, purple]
+color_index = 0
+
 screen = pygame.display.set_mode( (400, 300) )
 
+#font = pygame.font.SysFont("Arial", 100)
+#text = font.render("abc", True, (100,200,255))
 # Main loop. Your game would go inside this loop
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        print(event)
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+        if event.type == 4:
+            color_index += 1
+            if color_index >= len(colors):
+                color_index = 0
+    screen.fill(colors[color_index])
+    #screen.fill((0,0,0))
+    #screen.blit(text, pygame.mouse.get_pos())
+    pygame.display.flip()
