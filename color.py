984b34592c7cf203cfabf265a189baab130701a5
Rachel Heeren <rheeren@ucvts.org>
2019-12-12 10:47:52 -0500

    color.py and text.py

--- color.py
+++ color.py
@@ -15,10 +15,6 @@ colors = [red, orange, yellow, green, blue, purple]
 color_index = 0
 
 screen = pygame.display.set_mode( (400, 300) )
-x = 0
-y = 0
-font = pygame.font.SysFont("Arial", 100)
-text = font.render("abc", True, (100,200,255))
 # Main loop. Your game would go inside this loop
 while True:
     # do something for each event in the event queue (list of things that happen)
@@ -33,5 +29,4 @@ while True:
             if color_index >= len(colors):
                 color_index = 0
     screen.fill(colors[color_index])
-    screen.blit(text, pygame.mouse.get_pos())
     pygame.display.flip()
